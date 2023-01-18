43cd5b340926378beddb3c17aa53c1528e79af7d
Matt Ruwe <matthew.ruwe@kiewit.com>
2023-01-18 09:17:22 -0600

    Implicit conversation operators

---
+++ ImplicitConversionOperators/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitConversionOperators
+{
+    public abstract class Car
+    {
+        public abstract int TopSpeed { get; }
+
+        public static implicit operator Car(string typeOfCar)
+        {
+            switch (typeOfCar.ToLower())
+            {
+                case "bmw":
+                    return new Bmw();
+                case "4series":
+                    return new FourSeries();
+                case "honda":
+                    return new Honda();
+                case "prologue":
+                    return new Prologue();
+                default:
+                    throw new ArgumentException("Invalid car type");
+            }
+        }
+    }
+
+    public class Bmw : Car
+    {
+        public override int TopSpeed => 200;
+
+        public virtual int NumberOfDoors => 4;
+    }
+
+    public class FourSeries : Bmw
+    {
+        public override int NumberOfDoors => 2;
+    }
+
+    public class Honda : Car
+    {
+        public override int TopSpeed => 100;
+
+        public virtual bool IsElectric => false;
+    }
+
+    public class Prologue : Honda
+    {
+        public override bool IsElectric => true;
+    }
+}
---
+++ ImplicitConversionOperators/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace ImplicitConversionOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass1 = ClassConfiguration.ConfigOne;
+            Console.WriteLine(myClass1.ClassConfig);
+
+            Console.WriteLine("----");
+
+            MyClass myClass2 = "Two"; // string -> MyClass
+            ClassConfiguration classConfiguration2 = myClass2; // MyClass -> ClassConfiguration
+            Console.WriteLine(classConfiguration2);
+
+            Console.WriteLine("----");
+
+            Car honda = "Honda"; // string -> Honda
+            Console.WriteLine($"Type of car: {honda.GetType().Name}");
+            Console.WriteLine($"Top speed:   {honda.TopSpeed}");
+            Console.WriteLine($"Is Electric: {((Honda)honda).IsElectric}");
+
+            Console.WriteLine("----");
+
+            Car prologue = "prologue"; // string -> Prologue
+            Console.WriteLine($"Type of car: {prologue.GetType().Name}");
+            Console.WriteLine($"Top speed:   {prologue.TopSpeed}");
+            Console.WriteLine($"Is Electric: {((Prologue)prologue).IsElectric}");
+        }
+
+        public static bool Try()
+        {
+            Console.WriteLine(1);
+            return true;
+        }
+    }
+
+
+
+    public class MyClass
+    {
+        public static implicit operator ClassConfiguration(MyClass myClass)
+        {
+            switch (myClass.ClassConfig)
+            {
+                case "One":
+                    return ClassConfiguration.ConfigOne;
+                case "Two":
+                    return ClassConfiguration.ConfigTwo;
+                default:
+                    throw new InvalidOperationException("Invalid class configuration");
+            }
+        }
+
+        public static implicit operator MyClass(ClassConfiguration classConfig)
+        {
+            switch (classConfig)
+            {
+                case ClassConfiguration.ConfigOne:
+                    return new MyClass() { ClassConfig = "One" };
+                case ClassConfiguration.ConfigTwo:
+                    return new MyClass() { ClassConfig = "Two" };
+                default:
+                    return new MyClass() { ClassConfig = "Unknown" };
+            }
+        }
+
+        public static implicit operator MyClass(string classConfig)
+        {
+            return new MyClass() { ClassConfig = classConfig };
+        }
+
+        public string ClassConfig { get; private set; }
+    }
+
+    public enum ClassConfiguration
+    {
+        ConfigOne,
+        ConfigTwo
+    }
+}
