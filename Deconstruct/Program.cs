0c11500655b13adb17b0619a4dd6ea2c839c4cfc
Matt Ruwe <matthew.ruwe@kiewit.com>
2023-01-26 16:00:14 -0600

    cleanup

--- Deconstruct/Program.cs
+++ Deconstruct/Program.cs
@@ -1,34 +1,36 @@
 ﻿using Deconstruct;
 
-//User Defined Deconstructing
-var car = new Car(new Tire() { Width = 235, AspectRatio = 65, Diameter = 17 }) { Make = "Tesla", Model = "Model 3" };
 
-(int width, int aspectRatio, int diameter) = car;
-(string make, string model) = car;
+#region stuff
+////User Defined Deconstructing
+//var car = new Car(new Tire() { Width = 235, AspectRatio = 65, Diameter = 17 }) { Make = "Tesla", Model = "Model 3" };
 
-
-Console.WriteLine(width); // 235
-Console.WriteLine(aspectRatio); // 65
-Console.WriteLine(diameter); // 17
-Console.WriteLine(make); // Tesla
-Console.WriteLine(model); // Model 3
-
-//Record deconstructing
-(int tireWidth, int tireAspectRatio, int tireDiameter) = car.Tire;
-
-Console.WriteLine(tireWidth);
-Console.WriteLine(tireAspectRatio);
-Console.WriteLine(tireDiameter);
+//(int width, int aspectRatio, int diameter) = car;
+//(string make, string model) = car;
 
 
-//Extension method for system types
-var dictionary = new Dictionary<string, string>() { { "key1", "value1" }, { "key1", "value2" } };
-foreach (var (key, value) in dictionary)
-{
-    Console.WriteLine(key);
-    Console.WriteLine(value);
-}
+//Console.WriteLine(width); // 235
+//Console.WriteLine(aspectRatio); // 65
+//Console.WriteLine(diameter); // 17
+//Console.WriteLine(make); // Tesla
+//Console.WriteLine(model); // Model 3
 
+////Record deconstructing
+//(int tireWidth, int tireAspectRatio, int tireDiameter) = car.Tire;
+
+//Console.WriteLine(tireWidth);
+//Console.WriteLine(tireAspectRatio);
+//Console.WriteLine(tireDiameter);
+
+
+////Extension method for system types
+//var dictionary = new Dictionary<string, string>() { { "key1", "value1" }, { "key1", "value2" } };
+//foreach (var (key, value) in dictionary)
+//{
+//    Console.WriteLine(key);
+//    Console.WriteLine(value);
+//}
+#endregion
 
 
 namespace Deconstruct
@@ -46,18 +48,20 @@ namespace Deconstruct
 
         public Tire Tire { get; set; }
 
-        public void Deconstruct(out int width, out int aspectRation, out int diameter)
-        {
-            width = Tire.Width;
-            aspectRation = Tire.AspectRatio;
-            diameter = Tire.Diameter;
-        }
+        #region Stuff
+        //public void Deconstruct(out int width, out int aspectRation, out int diameter)
+        //{
+        //    width = Tire.Width;
+        //    aspectRation = Tire.AspectRatio;
+        //    diameter = Tire.Diameter;
+        //}
 
-        public void Deconstruct(out string make, out string model)
-        {
-            make = Make;
-            model = Model;
-        }
+        //public void Deconstruct(out string make, out string model)
+        //{
+        //    make = Make;
+        //    model = Model;
+        //}
+        #endregion
     }
 
     public record Tire(int Width, int AspectRatio, int Diameter);
