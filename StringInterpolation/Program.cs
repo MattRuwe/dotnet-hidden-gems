cd59473f97f15df0a22550de3b91bfc9ab93ae45
Matt Ruwe <matthew.ruwe@kiewit.com>
2023-01-26 15:29:04 -0600

    Added more meaningful example

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var firstName = "Matt";
+            var lastName = "Ruwe";
+            FormattableString fString = $"Hello, {firstName} {lastName}!";
+
+            Console.WriteLine(fString.Format);
+            var i = 0;
+            foreach (var argument in fString.GetArguments())
+            {
+                Console.WriteLine($"  Argument {++i}: {argument}");
+            }
+
+            Console.WriteLine("----");
+            Console.WriteLine(fString);
         }
     }
 }
