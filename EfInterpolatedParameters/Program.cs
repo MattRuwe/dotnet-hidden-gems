54f1b4d7b1193fe6641bcfa365e970e969833285
Matt Ruwe <matthew.ruwe@kiewit.com>
2023-01-26 15:52:45 -0600

    Cleanup

--- EfInterpolatedParameters/Program.cs
+++ EfInterpolatedParameters/Program.cs
@@ -4,20 +4,23 @@ namespace EfInterpolatedParameters
 {
     internal class Program
     {
-        // Things to show:
-        // Developer command line - ILDASM - Show the IL for the interpolated string
-        // Show how to use Sqlite with entity framework
-        // Show Logto in entity framework
-
         static void Main(string[] args)
         {
-            var context = new BlogContext();
-            
-            var blogId = 1;
-            FormattableString query = $"SELECT * FROM Posts WHERE BlogId = {blogId}";
-            Console.WriteLine(query.Format);
-            Console.WriteLine(query.GetArgument(0));
-            var posts = context.Posts.FromSqlInterpolated(query).ToList();
+            #region Stuff
+
+            // Things to show:
+            // Developer command line - ILDASM - Show the IL for the interpolated string
+            // Show how to use Sqlite with entity framework
+            // Show Log to in entity framework
+            //var context = new BlogContext();
+            //context.Database.EnsureCreated();
+
+            //var blogId = 1;
+            //FormattableString query = $"SELECT * FROM Posts WHERE BlogId = {blogId}";
+            //Console.WriteLine(query.Format);
+            //Console.WriteLine(query.GetArgument(0));
+            //var posts = context.Posts.FromSqlInterpolated(query).ToList();
+            #endregion
         }
     }
 }
