83ba8ea18e0a321a934297549bb463a0de7c7afe
Matt Ruwe <matthew.ruwe@kiewit.com>
2023-01-26 15:53:51 -0600

    Cleanup

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -2,24 +2,23 @@
 {
     internal class Program
     {
-        //https://github.com/dotnet/corert/blob/c6af4cfc8b625851b91823d9be746c4f7abdc667/src/System.Private.CoreLib/shared/System/FormattableString.cs
-
         static void Main(string[] args)
         {
             #region Stuff
-            var firstName = "Matt";
-            var lastName = "Ruwe";
-            FormattableString fString = $"Hello, {firstName} {lastName}!";
+            //https://github.com/dotnet/corert/blob/c6af4cfc8b625851b91823d9be746c4f7abdc667/src/System.Private.CoreLib/shared/System/FormattableString.cs
+            //var firstName = "Matt";
+            //var lastName = "Ruwe";
+            //FormattableString fString = $"Hello, {firstName} {lastName}!";
 
-            Console.WriteLine(fString.Format);
-            var i = 0;
-            foreach (var argument in fString.GetArguments())
-            {
-                Console.WriteLine($"  Argument {++i}: {argument}");
-            }
+            //Console.WriteLine(fString.Format);
+            //var i = 0;
+            //foreach (var argument in fString.GetArguments())
+            //{
+            //    Console.WriteLine($"  Argument {++i}: {argument}");
+            //}
 
-            Console.WriteLine("----");
-            Console.WriteLine(fString);
+            //Console.WriteLine("----");
+            //Console.WriteLine(fString);
             #endregion
         }
     }
