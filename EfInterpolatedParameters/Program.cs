29dd3d1887bb85d2158e4733cce3d04beca7d001
matt ruwe <matt@ruwe.net>
2025-07-07 17:17:04 -0500

    Cleaned up project.  Updated NuGet packages.

--- EfInterpolatedParameters/Program.cs
+++ EfInterpolatedParameters/Program.cs
@@ -12,15 +12,34 @@ namespace EfInterpolatedParameters
             // Developer command line - ILDASM - Show the IL for the interpolated string
             // Show how to use Sqlite with entity framework
             // Show Log to in entity framework
-            //var context = new BlogContext();
-            //context.Database.EnsureCreated();
+            var context = new BlogContext();
+            context.Database.EnsureCreated();
 
-            //var blogId = 1;
-            //FormattableString query = $"SELECT * FROM Posts WHERE BlogId = {blogId}";
-            //Console.WriteLine(query.Format);
-            //Console.WriteLine(query.GetArgument(0));
-            //var posts = context.Posts.FromSqlInterpolated(query).ToList();
+            UsingNormalFromSqlInterpolated(context);
+
+            UsingFormattableString(context);
+
             #endregion
+        }
+
+        private static void UsingNormalFromSqlInterpolated(BlogContext context)
+        {
+            // More expressive query using a FormattableString
+            var blogId = 1;
+            var posts = context.Posts.FromSqlInterpolated($"SELECT * FROM Posts WHERE BlogId = {blogId}").ToList();
+        }
+
+        private static void UsingFormattableString(BlogContext context)
+        {
+            // More expressive query using a FormattableString
+            var blogId = 1;
+            FormattableString query = $"SELECT * FROM Posts WHERE BlogId = {blogId}";
+            var previousConsoleColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Format:        {query.Format}");
+            Console.WriteLine($"Query Value 0: {query.GetArgument(0)}");
+            Console.ForegroundColor = previousConsoleColor;
+            var posts = context.Posts.FromSqlInterpolated(query).ToList();
         }
     }
 }
