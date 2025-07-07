f025b647b0c68292833b28144493ec6f460a27d3
matt ruwe <matt@ruwe.net>
2025-07-07 11:52:56 -0500

    Cleaned up the deconstruction examples

--- Deconstruct/Program.cs
+++ Deconstruct/Program.cs
@@ -2,65 +2,72 @@
 
 
 #region stuff
-////User Defined Deconstructing
-//var car = new Car(new Tire() { Width = 235, AspectRatio = 65, Diameter = 17 }) { Make = "Tesla", Model = "Model 3" };
+//User Defined Deconstruction
+var car = new Car(new Tire(235,65, 17)) { Make = "Tesla", Model = "Model 3" };
 
-//(int width, int aspectRatio, int diameter) = car;
-//(string make, string model) = car;
+(int width, int aspectRatio, int diameter) = car;
+(string make, string model) = car;
 
-
-//Console.WriteLine(width); // 235
-//Console.WriteLine(aspectRatio); // 65
-//Console.WriteLine(diameter); // 17
-//Console.WriteLine(make); // Tesla
-//Console.WriteLine(model); // Model 3
-
-////Record deconstructing
-//(int tireWidth, int tireAspectRatio, int tireDiameter) = car.Tire;
-
-//Console.WriteLine(tireWidth);
-//Console.WriteLine(tireAspectRatio);
-//Console.WriteLine(tireDiameter);
+Console.WriteLine("** User Defined Deconstruction:");
+Console.WriteLine($" width:       {width}"); // 235
+Console.WriteLine($" aspectRatio: {aspectRatio}"); // 65
+Console.WriteLine($" diameter:    {diameter}"); // 17
+Console.WriteLine($" make:        {make}"); // Tesla
+Console.WriteLine($" model:       {model}"); // Model 3
+Console.WriteLine();
 
 
-////Extension method for system types
-//var dictionary = new Dictionary<string, string>() { { "key1", "value1" }, { "key1", "value2" } };
-//foreach (var (key, value) in dictionary)
-//{
-//    Console.WriteLine(key);
-//    Console.WriteLine(value);
-//}
+//Record deconstruction
+(int tireWidth, int tireAspectRatio, int tireDiameter) = car.Tire;
+Console.WriteLine("** Record Deconstruction:");
+Console.WriteLine($" tireWidth:       {tireWidth}");
+Console.WriteLine($" tireAspectRatio: {tireAspectRatio}");
+Console.WriteLine($" tireDiameter:    {tireDiameter}");
+Console.WriteLine();
+
+
+//System type deconstruction
+Console.WriteLine("** System Type Deconstruction");
+var dictionary = new Dictionary<string, string>() { { "key1", "value1" }, { "key2", "value2" } };
+foreach (var (key, value) in dictionary)
+{
+    Console.WriteLine($" {key} = {value}");
+}
+Console.WriteLine();
 #endregion
 
 
 namespace Deconstruct
 {
 
-    public class Car
+    public class Car(Tire tire)
     {
-        public string Make { get; set; }
-        public string Model { get; set; }
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public Tire Tire { get; set; } = tire;
 
-        public Car(Tire tire)
+        #region Deconstruct methods
+        public void Deconstruct(out int width, out int aspectRatio, out int diameter)
         {
-            Tire = tire;
+            width = Tire.Width;
+            aspectRatio = Tire.AspectRatio;
+            diameter = Tire.Diameter;
         }
 
-        public Tire Tire { get; set; }
+        public void Deconstruct(out string make, out string model)
+        {
+            make = Make;
+            model = Model;
+        }
 
-        #region Stuff
-        //public void Deconstruct(out int width, out int aspectRation, out int diameter)
-        //{
-        //    width = Tire.Width;
-        //    aspectRation = Tire.AspectRatio;
-        //    diameter = Tire.Diameter;
-        //}
-
-        //public void Deconstruct(out string make, out string model)
-        //{
-        //    make = Make;
-        //    model = Model;
-        //}
+        public void Deconstruct(out string make, out string model, out int width, out int aspectRatio, out int diameter)
+        {
+            make = Make;
+            model = Model;
+            width = Tire.Width;
+            aspectRatio = Tire.AspectRatio;
+            diameter = Tire.Diameter;
+        }
         #endregion
     }
 
