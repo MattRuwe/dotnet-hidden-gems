84ed549c0362e57398a52c70adb2aafafbb35256
matt ruwe <matt@ruwe.net>
2025-07-08 09:45:55 -0500

    Updated example

--- StringInterpolation/Program.cs
+++ StringInterpolation/Program.cs
@@ -5,20 +5,22 @@
         static void Main(string[] args)
         {
             #region Stuff
-            //https://github.com/dotnet/corert/blob/c6af4cfc8b625851b91823d9be746c4f7abdc667/src/System.Private.CoreLib/shared/System/FormattableString.cs
-            //var firstName = "Matt";
-            //var lastName = "Ruwe";
-            //FormattableString fString = $"Hello, {firstName} {lastName}!";
+            // https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/FormattableString.cs
+            var firstName = "Matt";
+            var lastName = "Ruwe";
+            string regularString = $"Hello, {firstName} {lastName}!";
+            FormattableString fString = $"Hello, {firstName} {lastName}!";
+            
+            Console.WriteLine(fString.Format);
+            var i = 0;
+            foreach (var argument in fString.GetArguments())
+            {
+                Console.WriteLine($"  Argument {++i}: {argument}");
+            }
 
-            //Console.WriteLine(fString.Format);
-            //var i = 0;
-            //foreach (var argument in fString.GetArguments())
-            //{
-            //    Console.WriteLine($"  Argument {++i}: {argument}");
-            //}
-
-            //Console.WriteLine("----");
-            //Console.WriteLine(fString);
+            Console.WriteLine("----");
+            Console.WriteLine(fString);
+            Console.WriteLine(regularString);
             #endregion
         }
     }
